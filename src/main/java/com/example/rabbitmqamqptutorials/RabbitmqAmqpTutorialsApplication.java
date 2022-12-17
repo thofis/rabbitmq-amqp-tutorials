4f3f6b931fa311ff59f6be2de168bcf89a2194e1
Thomas Fischer <thomas.fischer@media-nox.de>
2022-12-17 14:16:14 +0100

    initial commit

---
+++ src/main/java/com/example/rabbitmqamqptutorials/RabbitmqAmqpTutorialsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqamqptutorials;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAmqpTutorialsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqAmqpTutorialsApplication.class, args);
+	}
+
+}
